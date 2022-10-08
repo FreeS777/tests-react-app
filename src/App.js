458a4e2eaed4e11171e010216a659eb6f66eb115
Artem Smirnov <frees.comp@gmail.com>
2022-10-08 17:29:33 +0300

    add counter app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './index.scss';
+import React from "react";
+import "./index.scss";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const setCountMinus = () => {
+    setCount(count - 1);
+  };
+
+  const setCountPlus = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button onClick={setCountMinus} className="minus">
+          - Minus
+        </button>
+        <button onClick={setCountPlus} className="plus">
+          Plus +
+        </button>
       </div>
     </div>
   );
